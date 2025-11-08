9c52b9fa5c4b0f8231c44c2ae4c35c2a6f98650c
Jesse Sher <jebba272727@gmail.com>
2025-11-08 15:09:51 +1100

    Yes! it works

--- main.cpp
+++ main.cpp
@@ -7,10 +7,16 @@ extern "C" {
 #include <iostream>
 
 int main() {
- lua_State* L = luaL_newstate();
-luaL_openlibs(L);
-luaL_dostring(L, "print('Hello from Lua in WASM!')");
-lua_close(L);
+    lua_State* L = luaL_newstate();      // Create new Lua state
+    luaL_openlibs(L);                    // Open standard Lua libraries
 
+    // Run a Lua script string
+    if (luaL_dostring(L, "print('Hello, World from Lua in WASM!')")) {
+        // If error occurs
+        std::cerr << "Lua error: " << lua_tostring(L, -1) << std::endl;
+        lua_pop(L, 1);
+    }
+
+    lua_close(L); // Close Lua state
     return 0;
 }
